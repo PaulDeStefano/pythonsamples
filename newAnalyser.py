0c9b5429fa1117b7708b42c61209f712d235b805
Paul R. DeStefano <paul (dot) destefano (ta)willamettealumni (dot) c o m>
2013-09-09 10:07:02 -0700

    Starting to take shape

--- newAnalyser.py
+++ newAnalyser.py
@@ -21,7 +21,22 @@ import argparse
 import sys
 import pandas
 import re
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+# use tex to format text in matplotlib
+#rc('text', usetex=True)
+mpl.rc('figure'
+        ,dpi=100 )
+mpl.rc('axes'
+        ,grid=True)
+mpl.rc('lines'
+        ,linestyle=None
+        ,marker='+')
+mpl.rc('xtick'
+        ,direction='out' )
+mpl.rc('ytick'
+        ,direction='out' )
 
 def logMsg(s, *args):
     #print("DEBUG: logMsg called")
@@ -67,7 +82,7 @@ class tofAnalayser:
         self.parser = argparse.ArgumentParser(description='New TIC data plotter and analyser.')
         parser = self.parser
 
-        parser.add_argument('fileList', nargs='+', help='TIC data files')
+        parser.add_argument('fileList', nargs='*', help='TIC data files')
         parser.add_argument('--desc', nargs='?', help='Description (title) for plots', default='DEFAULT DESCRIPTION')
         parser.add_argument('-g', '--gap', nargs='?', default='3', help='Maximum gap/tolerance (s) between epochs')
         parser.add_argument('--offsets', nargs='?', help='File continating xPPSoffset values (from sbf2offset.py)')
@@ -81,6 +96,7 @@ class tofAnalayser:
         parser.add_argument('--fft', '-S', action='store_true',default=False, help='Calculate Spectral Density (Fourier Transform) of any time series')
         parser.add_argument('--corr', '-C', nargs='?', help='File continating "correction" values (from getCorrData.sh)')
         parser.add_argument('--showFormats', action='store_true', default=False, help='Show a list of known formats')
+        parser.add_argument('--loadSaved', nargs='?', default=False, help='Load a previously saved data set from this file')
 
         self.options = parser.parse_args()
         args = self.options
@@ -99,6 +115,11 @@ class tofAnalayser:
         optionsDict['doFT'] = args.fft
         optionsDict['corr'] = args.offsets
         optionsDict['showFormats'] = args.showFormats
+        optionsDict['loadSaved'] = args.loadSaved
+
+        ''' we may not always want to do the same calcuations.  If data has been
+        loaded from stored data file, then some processing can be skipped.'''
+        optionsDict['reProcess'] = True
 
         logMsg('DEBUG: harvested coniguration:',optionsDict)
 
@@ -136,7 +157,13 @@ class tofAnalayser:
 
     def loadData(self):
         optionsDict = self.optionsDict
-        if 'inputFiles' in self.optionsDict:
+        if self.options.loadSaved :
+            self.__loadSaved()
+            self.optionsDict['reProcess'] = False
+            logMsg("DEBUG: reProcess: ",self.optionsDict['reProcess'])
+
+        logMsg("DEBUG: intputFiles: ",self.optionsDict['inputFiles'])
+        if 'inputFiles' in self.optionsDict and self.optionsDict['inputFiles'] != []:
             """ load files at end, assume default format:
             utc dT ...
             """
@@ -148,9 +175,14 @@ class tofAnalayser:
                 #newData.tz_localize('UTC')
                 self.addData(newData, loc=loc)
 
+            if not self.optionsDict['reProcess']:
+                '''we already loaded some saved data from, so reverse the flag
+                that prevents re-processing the data because more data has been
+                added and we should start over'''
+                self.optionsDict['reProcess'] = True
+
         else:
-            logMsg("ERROR: no files to load")
-            return 1
+            logMsg("WARNING: no files to load")
 
         """
         if 'offsetFile' in self.optionsDict :
@@ -162,6 +194,7 @@ class tofAnalayser:
                 loc, fileName, fmt = s.split(':')
                 newData = self._loadFile(fileName,fmt='getCorrData')
         """
+        logMsg("DEBUG: reProcess: ",self.optionsDict['reProcess'])
 
     def _getFormat(self,fmt):
         if fmt in self.formatDict:
@@ -196,6 +229,7 @@ class tofAnalayser:
                 ,delim_whitespace = delim_whitespace
                 ,usecols = usecols
                 ,nrows = self.optionsDict['importLimit']
+                ,comment = '#'
                 )
 
         logMsg('DEBUG: _loadFile: loading file...done.')
@@ -209,12 +243,16 @@ class tofAnalayser:
         logMsg('DEBUG: previewing...')
         logMsg('DEBUG: ', self.dbDict.keys() )
         for loc in self.dbDict.keys():
-            logMsg('DEBUG: preview: loc=',loc,'\n', self.dbDict[loc].describe() )
+            dat = self.dbDict[loc]
+            if dat.empty:
+                logMsg("DEBUG: empty data for location:",loc,"skipping")
+                continue
+            logMsg('DEBUG: preview: loc=',loc,'\n', dat.describe() )
             #logMsg('DEBUG: preview: loc=',loc,'\n', self.dbDict[loc].head() )
-            previewDF = self.dbDict[loc]
+            previewDF = dat
             print previewDF.head(10)
             print previewDF.tail(10)
-            previewDF.plot()
+            previewDF.plot(title=loc+':'+self.options.desc)
             plt.show()
 
         logMsg('DEBUG: previewing...done')
@@ -223,40 +261,109 @@ class tofAnalayser:
         '''fix up stuff before other calculations'''
         '''dT is in seconds, but we're interested in nanosecond level differences.
         And, besides, it will be easiest to convert to a common unit.'''
-        db = self.dbDict
-        for loc in db.keys():
+        if not self.optionsDict['reProcess'] :
+            ''' skip this stuff'''
+            return None
+
+        for loc in self.dbDict.keys():
+            dat = self.dbDict[loc]
+            if dat.empty:
+                logMsg("DEBUG: empty data for location:",loc,"skipping")
+                continue
             dat = self.dbDict[loc]
             '''convert dT to ns'''
             if 'dT' in dat.keys():
-                dat['dT_ns'] = dat.dT * 1E9
+                '''drop meas > 1s'''
+                df = dat['dT']
+                df = df[df < 1.01]
+                dat['dT'] = df
+                '''convert to ns'''
+                df = df * 1E9
+                dat['dT_ns'] = df
 
             '''convert rxClkBias to ns'''
             if 'rxClkBias' in dat.keys():
                 dat['rxClkBias_ns'] = dat['rxClkBias'] * 1E6
 
     def doXPPScorr(self):
+        logMsg("DEBUG: reProcess: ",self.optionsDict['reProcess'])
+        if not self.optionsDict['reProcess'] :
+            return None
+
         logMsg('DEBUG: xPPScorr...')
 # apply xppsoffset
         for loc in self.dbDict.keys():
-            db = self.dbDict[loc]
-            if ('dT_ns' not in db.keys()) or ('xPPSOffset' not in db.keys()):
-                raise Exception("Data needed for xPPSOffset corrections not found")
             dat = self.dbDict[loc]
-            dat['dTCorr'] = dat.dT_ns + self.options.negOffset * dat.xPPSOffset 
-            dat[:,['dT_ns','dTCorr']].plot()
+            if dat.empty:
+                logMsg("DEBUG: empty data for location:",loc,"skipping")
+                continue
+            if ('dT_ns' not in dat.keys()) or ('xPPSOffset' not in dat.keys()):
+                raise Exception("Data needed for xPPSOffset corrections not found")
+            coeff = -1
+            if not self.options.negOffset:
+                coeff = 1
+            dat['dTCorr'] = dat.dT_ns + coeff * dat.xPPSOffset 
+            dat[['dT_ns','dTCorr']].plot(title=loc+':'+self.options.desc,grid=True)
             plt.show()
         logMsg('DEBUG: xPPScorr...done')
 
     def doPPPcorr(self):
 # find PPP Correction & apply
+        if not self.optionsDict['reProcess'] :
+            return None
+
         logMsg('DEBUG: PPCorr...')
         for loc in self.dbDict.keys():
             dat = self.dbDict[loc]
-            dat['PPCorr'] = dat.rxClkBias - dat.rx_clk_ns
-            dat['dTPPCorr'] = dat.dT_ns - dat.PPCorr
-            dat[:,['dT_ns','PPCorr','dTPPCorr']].plot()
+            if dat.empty:
+                logMsg("DEBUG: empty data for location:",loc,"skipping")
+                continue
+            if ('rxClkBias_ns' not in dat.keys()) or ('rx_clk_ns' not in dat.keys()):
+                raise Exception("Data needed for PP corrections not found")
+            logMsg("DEBUG: loc:",loc,":",dat)
+            dat['PPCorr'] = dat.rxClkBias_ns - dat.rx_clk_ns
+            dat['dTPPCorr'] = dat.dTCorr - dat.PPCorr
+            dat[['dT_ns','PPCorr','dTPPCorr']].plot(title=loc+':'+self.options.desc)
             plt.show()
         logMsg('DEBUG: PPCorr...done')
+
+    def save(self):
+        logMsg('DEBUG: saving database to file...')
+        fileName = self.options.outputPrefix
+        if fileName:
+            logMsg("DEBUG: saving to HDF5 store in file",fileName)
+        #with pandas.HDFStore(fileName+'.hdf5') as store:
+            store = pandas.HDFStore(fileName+'.hdf5')
+            for loc in self.dbDict.keys():
+                store[loc] = self.dbDict[loc]
+            store.close()
+            del store
+        else:
+            logMsg("DEBUG: couldn't write, no outputFile specified")
+
+    def __loadSaved(self):
+        logMsg('DEBUG: loaded saved database from file...')
+        fileName = self.options.loadSaved
+        if fileName:
+            logMsg("DEBUG: loading saved data from HDF5 store in file",fileName)
+            store = pandas.HDFStore(fileName)
+        #with pandas.HDFStore(fileName) as store:
+            for loc in self.dbDict.keys():
+                storeKey = '/'+loc
+                logMsg("DEBUG: loading key",storeKey,"from HDF5 store into location",loc)
+                self.dbDict[loc] = store[storeKey]
+
+            store.close()
+        else:
+            logMsg("DEBUG: couldn't load, no loadSaved file specified")
+
+    def analyse(self):
+        logMsg('DEBUG: Analyse...')
+        for loc in self.dbDict.keys():
+            dat = self.dbDict[loc]
+            dat = dat.dropna(how='any')
+            logMsg("DEBUG: after dropna:",dat)
+        logMsg('DEBUG: Analyse...done')
 
 ## MAIN ##
 def runMain():
@@ -266,27 +373,28 @@ def runMain():
 # import main data
     tof.loadData()
 # preview
-    tof.preview()
+    #tof.preview()
 # organize data
     tof.prep()
-    tof.preview()
+    #tof.preview()
 # import correcitions
-    tof.doXPPScorr()
-    tof.preview()
 # apply corrections
+    tof.doXPPScorr()
+    #tof.preview()
     tof.doPPPcorr()
-    tof.preview()
-# analyse data
-    #tof.analyse()
-# overview plot
+    #tof.preview()
 # store analysed data
+    tof.save()
+# analyse data
+    tof.analyse()
+    tof.preview()
+# store analysed data
+    tof.save()
+# overview plot
 # find sub-series
 # reanalyse
 # replot
 
-
-# use tex to format text in matplolib
-#rc('text', usetex=True)
 
 if __name__ == "__main__" :
     try:
